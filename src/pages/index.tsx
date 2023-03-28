e6c6633bd02cf341bb49fac746f1e15b3737382f
Lucas Fernandes <lucas_matheus22@hotmail.com>
2023-03-28 17:51:10 -0300

    Cabeçalho da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,12 @@ import React from "react";
 
 const Button = styled("button", {
   backgroundColor: "$green300",
-  color: "White",
   borderRadius: 4,
   border: 0,
   padding: "4px 8px",
 
   "&:hover": {
-    filter: "brightness(0.9)",
+    filter: "brightness(0.8)",
   },
 });
 
